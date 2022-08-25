47a2488861640cb679bf5c1608140dab7ba3510a
mhira-codes <alhassansamiratu991@gmail.com>
2022-08-25 13:52:45 +0000

    header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,20 @@ import {IoIosArrowUp} from 'react-icons/io';
 import { useState } from 'react';
 const Header = () => {
 const [active, setActive] = useState('')
-let logo ="https://images.unsplash.com/photo-1639603683079-7398c604497a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJhbmtpbmclMjBsb2dvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-let navLinks = ["Overview","Payment", "Cards","Account", "Admin"]
-let userImage ="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
 
+let navLinks = ["Overview","Payment", "Cards","Account", "Admin"]
+const  userImage = require('../assets/profile-img.jpg')
+const LOGO = require('.././assets/header-logo.jpg')
   return (
     <div className='flex w-full items-center justify-between space-x-4 pb-3 pt-4 px-3'>
 <div className='border-b border-gray-900 w-[25%] flex items-center justify-center pb-2 md:justify-start  md:start-x-2  md:pb-6'>
-<img className='w-10 h-10 md:w-12 md:h-12 rounded-full' src={logo} alt='logo'/>
+<img className='w-10 h-10 md:w-12 md:h-12 rounded-full' src={LOGO} alt='logo'/>
 <h1 className='self-center hidden   md:inline md:font-bold md:text-xl'>OpenBank</h1>
 </div>
-<div className='border-b border-gray-900 w-[140%] flex items-center justify-center pb-5 space-x-2'>
+<div className='border-b border-gray-900 w-[140%] flex items-center justify-center pb-5 space-x-2' >
 {navLinks.map((link)=>(
-    <div className='flex justify-center items-center relative' onClick={()=> setActive(link)}>
-        <NavLinks title={link} key={link}/>
+    <div className='flex justify-center items-center relative' onClick={()=> setActive(link)} key={link}>
+        <NavLinks title={link}/>
         <div className={`${link === active ? "items-center justify-center flex absolute -bottom-3" :"hidden"}`}>
             <IoIosArrowUp/>
         </div>

eb2b0b45524f29d535f0efd72abc7cde62101244
mhira-codes <alhassansamiratu991@gmail.com>
2022-08-25 13:53:44 +0000

    update on app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App  h-screen overflow-y-scroll scrollbar-hide px-6 overflow-x-hidden">
 <Header/>
+<div className='flex items-start justify-start w-full space-x-2'>
+  <Navbar/>
+</div>
     </div>
   );
 }

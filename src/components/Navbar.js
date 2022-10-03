1f78cbf9d856e4ca30a0c3cc9ee2abe7b7b3231b
mhira-codes <alhassansamiratu991@gmail.com>
2022-10-03 15:10:04 +0000

    minor  changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,21 @@ const Navbar = () => {
         <Card type="mastercard" />
         <Card type="visa" />
       </div>
+      <div className="flex justify-center items-center w-full border-dashed border border-gray-400 rounded-lg py-3 my-2">
+<button className="w-full">Add Cart</button>
+      </div>
+
+      <div className="flex items-start flex-col justify-between w-full h-full space-y-4 px-3  pb-2 pt-[150px]">
+        <div className="flex flex-col items-start justify-center w-full space-y-4  pb-6 border-b  border-gray-900">
+          <a>Deposits</a>
+          <a>Loyalty Programs</a>
+          <a>Loans</a>
+        </div>
+        <div className="text-black space-y-3  ">
+          <h1 className="font-bold text-lg">OpenBank</h1>
+          <p className="text-sm">2022 openbank licence #19 from 06.07.2022</p>
+        </div>
+      </div>
     </div>
   );
 };
